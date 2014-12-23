f5599f2c9b894ca682f609d81c786cd3110b43ee
James Lal <james@lightsofapollo.com>
2014-12-23 00:29:17 -0800

    crud

--- src/collection.js
+++ src/collection.js
@@ -1,48 +1,93 @@
 import * as Joi from 'joi';
 import assert from 'assert';
 import denodeify from 'denodeify';
+import Doq from 'doqmentdb';
 
 let validate = denodeify(Joi.validate.bind(Joi));
+
+const COLLECTION_PROXY = [
+  'insert',
+  'getCollection',
+  'find',
+  'findOne',
+  'findAndRemove',
+  'findOneAndRemove',
+  'findAndModify',
+  'findOneAndModify',
+  'findOrCreate',
+  'update'
+];
+
+class Collection {
+  constructor(options) {
+    Joi.assert(options, Joi.object().unknown(false).keys({
+      client: Joi.object().required().description('doq client'),
+      schema: Joi.object().required().description('joi schema'),
+      id: Joi.string().required().description('id of the collection')
+    }), `Collection ${options.id || 'unknown collection'}`);
+    Object.assign(this, options);
+
+    for (let proxy of COLLECTION_PROXY) {
+      this[proxy] = this.client[proxy].bind(this.client);
+    }
+  }
+
+  async validateDocument(doc) {
+    return await validate(doc, this.schema);
+  }
+
+  async create(doc) {
+    doc = await this.validateDocument(doc);
+    return await this.client.insert(doc);
+  }
+}
 
 /**
 Purely an experiment in building documentdb models.
 */
-class Collection {
-  constructor(name) {
-    this.name = name;
+class CollectionBuilder {
+  constructor(id) {
+    this.id = id ;
   }
 
   schema(joi) {
     this.schema = joi;
     return this;
   }
-
-  async validate(input) {
-    if (!this.schema) return input;
-    return await validate(input, this.schema);
-  }
 }
 
 class Connection {
-  constructor(resource, client) {
-    this.resource = resource;
+  constructor(doq, client, database) {
+    this.doq = doq;
     this.client = client;
+    this.database = database;
   }
 
   async destroy() {
-    return await this.client.deleteDatabaseAsync(this.resource._self);
-  }
-
-  create() {
+    let db = await this.doq.getDatabase();
+    let deleteDb = denodeify(this.client.deleteDatabase.bind(this.client));
+    return await deleteDb(db._self);
   }
 }
 
+/**
+Initialize the connections optionally creating collections.
+*/
 export async function connect(client, database, collections) {
-  let db = await client.createDatabaseAsync({ id: database });
-  return new Connection(db.resource, client);
+  let doqclient = new Doq(client, database);
+  let con = new Connection(doqclient, client, database);
+  for (let col of collections) {
+    con[col.id] = new Collection({
+      id: col.id,
+      schema: col.schema,
+      client: doqclient.use(col.id)
+    });
+  }
+
+  return con;
 }
 
-export function define(name) {
-  Joi.assert(name, Joi.string().min(1));
-  return new Collection(name);
+export function define(id) {
+  Joi.assert(id, Joi.string().min(1));
+  return new CollectionBuilder(id);
 }
--- test/documentdb_test.js
+++ test/documentdb_test.js
@@ -1,4 +1,4 @@
-import * as documentdb from 'documentdb-q-promises';
+import * as documentdb from 'documentdb';
 import * as subject from '../src/collection';
 
 import * as Joi from 'joi';
@@ -6,39 +6,82 @@ import uuid from 'uuid';
 import assert from 'assert';
 
 let url = 'https://stage-treeherder-proxy.documents.azure.com:443/';
-let client = new documentdb.DocumentClientWrapper(url, {
+let client = new documentdb.DocumentClient(url, {
   masterKey: 'RMMBgZ7iNxcohzoc/ZgtB9dez0InyfpeomPP1F2G6dDcWwc4rwyQRsRdXL03Nd6VPKmW7dxd+SGrUqcXoj/DCA=='
 });
 
 suite('documentdb', function() {
   let Data = subject.define('repository').
     schema(Joi.object().keys({
+      id: Joi.string().min(1).required(),
       url: Joi.string().required(),
     }));
 
   let connection, db = uuid.v4();
-  setup(async function() {
+  suiteSetup(async function() {
     connection = await subject.connect(client, db, [
       Data
     ]);
   });
 
-  teardown(async function() {
+  suiteTeardown(async function() {
     await connection.destroy();
   });
 
-  test('create connection', async function() {
-    let dbs = await client.readDatabases().toArrayAsync();
-    let ids = dbs.feed.reduce(function(current, db) {
-      current[db.id] = db;
-      return current;
-    }, {});
-    assert(ids[db], 'database was created...');
+  test('create connection second time', async function() {
+    let con = await subject.connect(client, db, [Data]);
   });
 
-  test('validate()', async function() {
-    await Data.validate({
-      url: 'http://github.com'
+  suite('collections', function() {
+    test('validateDocument() fail', async function() {
+      let err;
+      try {
+        await connection.repository.validateDocument({});
+      } catch (e) {
+        err = e;
+      }
+      assert(err);
+    });
+
+    test('validateDocument() pass', async function() {
+      await connection.repository.validateDocument({
+        id: 'wootbar',
+        url: 'http://github.com'
+      });
+    });
+
+    suite('CRUD', function() {
+      setup(async function() {
+        await connection.repository.create({
+          id: 'wootbar22',
+          url: 'baz'
+        });
+      });
+
+      teardown(async function() {
+        await connection.repository.findOneAndRemove({ id: 'wootbar22' });
+      });
+
+      test('findOne', async function() {
+        let result = await connection.repository.findOne({
+          id: 'wootbar22'
+        });
+
+        assert.equal(result.id, 'wootbar22');
+        assert.equal(result.url, 'baz');
+      });
+
+      test('update', async function() {
+        let doc = await connection.repository.findOne({ id: 'wootbar22' });
+        doc.url = 'xfoobar';
+        await connection.repository.update({ id: 'wootbar22' }, { url: 'xfoobar' });
+
+        let docUpdate = await connection.repository.findOne({ id: 'wootbar22' });
+        assert.equal(docUpdate.url, doc.url);
+        assert.equal(docUpdate._self, doc._self);
+        assert.notEqual(doc._etag, docUpdate._etag);
+      });
     });
   });
+
 });
