9459542203560b32ce53ab7c86bb3fab9174282b
James Lal <james@lightsofapollo.com>
2014-12-27 01:08:09 -0800

    Add helper which mimics pushlog

---
+++ test/repository_monitor/pushlog.js
@@ -0,0 +1,75 @@
+import Hapi from 'hapi';
+import denodeify from 'denodeify';
+
+// Default number of recent pushes to return if no arguments passed..
+const DEFAULT_PUSHES = 10;
+
+function intOrUndefined(n) {
+  if (typeof n === 'string') return parseInt(n, 10);
+  return n;
+}
+
+class Pushlog {
+  constructor(server) {
+    let server = new Hapi.Server();
+    server.connection({ port: 0 });
+    server.route({
+      method: 'GET',
+      path: '/json-pushes/',
+      handler: this.pushlog.bind(this)
+    });
+
+    this.server = server;
+    this.lastpushid = -1;
+    this.pushes = {};
+    this.push();
+  }
+
+  push(changesets={}, date=new Date(), user="xfoo@bar.com") {
+    let push = this.pushes[++this.lastpushid] = {};
+    push.changesets = changesets;
+    push.date = date || new Date()
+    push.date = (new Date(push.date).valueOf()) / 1000;
+    push.user = user;
+    return this;
+  }
+
+  async pushlog(request, reply) {
+    let response = { lastpushid: this.lastpushid }
+    let pushes = {};
+
+    let endID = intOrUndefined(request.query.endID) || this.lastpushid;
+    let startID = intOrUndefined(request.query.startID);
+
+    if (!startID) {
+      startID = endID - DEFAULT_PUSHES;
+      if (startID < 0) {
+        startID = 0;
+      }
+    }
+
+    // Original logic of how this should work is documented at:
+    // http://mozilla-version-control-tools.readthedocs.org/en/latest/hgmo/pushlog.html
+    for (let pushid = startID + 1; pushid <= endID; pushid++) {
+      if (!(pushid in this.pushes)) continue;
+      pushes[pushid] = this.pushes[pushid];
+    }
+
+    reply({ lastpushid: this.lastpushid, pushes });
+  }
+
+  async start() {
+    await denodeify(this.server.start.bind(this.server))();
+    this.url = `http://localhost:${this.server.info.port}`
+  }
+
+  async stop() {
+    return await denodeify(this.server.stop.bind(this.server))();
+  }
+}
+
+export default async function() {
+  let server = new Pushlog();
+  await server.start();
+  return server;
+}
---
+++ test/repository_monitor/pushlog_test.js
@@ -0,0 +1,48 @@
+import pushlog from './pushlog'
+import request from 'superagent-promise';
+import denodeify from 'denodeify';
+import assert from 'assert';
+
+suite('pushlog', function() {
+  let server;
+  suiteSetup(async function() {
+    server = await pushlog();
+  });
+
+  suiteTeardown(async function() {
+    await server.stop();
+  });
+
+  async function get(query={}) {
+    let qs = Object.assign({}, query);
+    let path = `${server.url}/json-pushes/`
+    let res = await request.get(path).query(query).end();
+    return res.body;
+  }
+
+  test('full+v2', async function() {
+    let res = await get({ version: 2, full: 1 });
+    assert.equal(res.lastpushid, 0);
+  });
+
+  test('limit pushes', async function() {
+    let a = { a: true };
+    let b = { b: true };
+    let c = { c: true };
+
+    [a, b, c].forEach((value) => { server.push(value); });
+
+    let all = await get();
+    assert.equal(all.lastpushid, 3);
+
+    let fetchB = await get({ startID: 1, endID: 2 });
+    assert.deepEqual(fetchB.pushes[2].changesets, b);
+    assert.deepEqual(Object.keys(fetchB.pushes), ['2']);
+
+    let fetchBC = await get({ startID: 1 });
+    assert.deepEqual(Object.keys(fetchBC.pushes), ['2', '3']);
+
+    let fetchABC = await get({ startID: 0, endID: 3 });
+    assert.deepEqual(Object.keys(fetchABC.pushes), ['1', '2', '3']);
+  });
+});
