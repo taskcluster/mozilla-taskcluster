6aa7b6956f241852f432f633764c246862a8706e
James Lal <james@lightsofapollo.com>
2015-01-13 00:41:53 -0800

    clear completed jobs (frees up redis memory)

--- src/bin/workers.js
+++ src/bin/workers.js
@@ -48,6 +48,18 @@ cli(async function main(runtime, config) {
   // Start interval promotion (should only run one of these)...
   runtime.jobs.promote();
 
+  // Clear any completed jobs from the redis queue we only care about errors...
+  runtime.jobs.on('job complete', function(id, result){
+    runtime.kue.Job.get(id, function(err, job) {
+      if (err) return;
+      job.remove(function(err){
+        if (err) {
+          console.error('error removing job:', err);
+        }
+      });
+    });
+  });
+
   // Process the incoming pushes....
   runtime.jobs.process('publish-push', 100, work(
     PublishPushJob,
