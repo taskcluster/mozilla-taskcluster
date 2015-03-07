849340ac6ca75ef5dada7df2b97d38d7fc2fa76f
James Lal <james@lightsofapollo.com>
2015-03-06 20:34:12 -0800

    Generate configs for documentdb too

--- test/circleci.js
+++ test/circleci.js
@@ -9,7 +9,15 @@ var yaml = require('js-yaml');
 assert(process.env.PULSE_USERNAME, 'has env.PULSE_USERNAME');
 assert(process.env.PULSE_PASSWORD, 'has env.PULSE_PASSWORD');
 
+assert(process.env.DOCUMENTDB_KEY, 'has env.DOCUMENTDB_KEY');
+assert(process.env.DOCUMENTDB_HOST, 'has env.DOCUMENTDB_HOST');
+
 var config = {
+  documentdb: {
+    host: process.env.DOCUMENTDB_HOST,
+    key: process.env.DOCUMENTDB_KEY
+  },
+
   treeherderTaskcluster: {
     connectionString: 'amqps://' + process.env.PULSE_USERNAME + ':' + 
                       process.env.PULSE_PASSWORD + '@pulse.mozilla.org:5671'
