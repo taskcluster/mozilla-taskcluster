4f7c34a72beb132469fe8c8e5ca9cfe4c0516593
James Lal <james@lightsofapollo.com>
2015-03-20 09:51:18 -0700

    Add utility to run graph creation steps locally

---
+++ src/bin/create_graph.js
@@ -0,0 +1,65 @@
+#! /usr/bin/env node
+/**
+Import repositories from treeherder proper and output the json structure the
+the proxy service uses...
+*/
+
+import 'babel/polyfill';
+import loadConfig from '../config';
+import createRuntime from '../runtime';
+
+import { ArgumentParser } from 'argparse';
+import TaskclusterGraphJob from '../jobs/taskcluster_graph';
+
+async function run(fn) {
+  let parser = new ArgumentParser();
+  parser.addArgument(['profile'], {
+    help: 'Configuration profile to use'
+  });
+
+  parser.addArgument(['repo'], {
+    help: 'Repository alias'
+  });
+
+  parser.addArgument(['pushid'], {
+    help: 'Pushlog id'
+  });
+
+  parser.addArgument(['revision_hash'], {
+    help: 'Treeherder revision hash'
+  });
+
+  try {
+    let args = parser.parseArgs();
+    let config = await loadConfig(process.argv[2]);
+    let runtime = await createRuntime(config);
+
+    await fn(runtime, config, args);
+  } catch (err) {
+    setTimeout(() => {
+      throw err;
+    });
+  }
+}
+
+run(async function main(runtime, config, args) {
+  let [repo] = await runtime.repositories.find({ alias: args.repo });
+  let { pushid, revision_hash } = args;
+
+  let job = new TaskclusterGraphJob({
+    config: config,
+    runtime: runtime
+  });
+
+  await job.work({
+    log: console.log.bind(console),
+    data: {
+      pushref: { id: pushid },
+      repo: repo,
+      revision_hash: revision_hash
+    }
+  });
+
+  process.exit();
+});
+
